c035363a17f9879e982b02cea93d13c92a8650b5
PULI VENKATA KIRAN REDDY <pulivenkatakiranreddy2006@gmail.com>
2026-03-10 08:44:13 +0530

    Add files via upload
    import java.util.HashMap;
    import java.util.Map;
    
    public class OOPSPrint {
    
        public static void main(String[] args) {
    
            Map<Character, String[]> patternMap = buildCharacterPatterns();
    
            renderBanner("OOPS", patternMap);
        }
    
        public static Map<Character, String[]> buildCharacterPatterns() {
    
            Map<Character, String[]> map = new HashMap<>();
    
            map.put('O', new String[]{
                    " ***** ",
                    "*     *",
                    "*     *",
                    "*     *",
                    "*     *",
                    "*     *",
                    " ***** "
            });
    
            map.put('P', new String[]{
                    "****** ",
                    "*     *",
                    "*     *",
                    "****** ",
                    "*      ",
                    "*      ",
                    "*      "
            });
    
            map.put('S', new String[]{
                    " ***** ",
                    "*      ",
                    "*      ",
                    " ***** ",
                    "      *",
                    "      *",
                    " ***** "
            });
    
            return map;
        }
    
        public static void renderBanner(String message, Map<Character, String[]> map) {
    
            for (int row = 0; row < 7; row++) {
    
                StringBuilder line = new StringBuilder();
    
                for (char ch : message.toCharArray()) {
    
                    String[] pattern = map.get(ch);
    
                    if (pattern != null) {
                        line.append(pattern[row]).append("   ");
                    }
                }
    
                System.out.println(line);
            }
        }
    }

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,68 @@
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
+import java.util.HashMap;
+import java.util.Map;
 
+public class OOPSPrint {
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildCharacterPatterns();
+
+        renderBanner("OOPS", patternMap);
+    }
+
+    public static Map<Character, String[]> buildCharacterPatterns() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
+    }
+
+    public static void renderBanner(String message, Map<Character, String[]> map) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+
+                String[] pattern = map.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("   ");
+                }
+            }
+
+            System.out.println(line);
+        }
     }
 }
